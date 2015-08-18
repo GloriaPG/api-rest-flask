a7a63d85798359c2352844a7402f1bcd2ea19d3c
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2015-08-18 00:35:40 -0500

    Add connection mysql

--- FLASK-RESTful/api.py
+++ FLASK-RESTful/api.py
@@ -1,15 +1,140 @@
-from flask import flask
-from flask.ext.restful import Api, Resource
+#!flask/bin/python
 
-app=Flask(__name__)
-api=Api(app)
+"""Alternative version of the ToDo RESTful server implemented using the
+Flask-RESTful extension."""
 
-class UserAPI(Resource)
-	def get(self,id)
-    	pass
-    def put(self,id)
-    	pass
-    def delete(self,id)
-    	pass
+from flask import Flask, jsonify, abort, make_response
+from flask.ext.restful import Api, Resource, reqparse, fields, marshal
+from flask.ext.httpauth import HTTPBasicAuth
+from flask import g
+from flask import Response
+from flask import request
+import json
+import MySQLdb
 
-api.add_resource(UserAPI,'/users/<int:id>', endpoint='user')
+app = Flask(__name__, static_url_path="")
+api = Api(app)
+auth = HTTPBasicAuth()
+
+@app.before_request
+def db_connect():
+  g.conn = MySQLdb.connect(host='localhost',
+                              user='test',
+                              passwd='password',
+                              db='test')
+  g.cursor = g.conn.cursor()
+
+@app.after_request
+def db_disconnect(response):
+  g.cursor.close()
+  g.conn.close()
+  return response
+
+def query_db(query, args=(), one=False):
+  g.cursor.execute(query, args)
+  rv = [dict((g.cursor.description[idx][0], value)
+  for idx, value in enumerate(row)) for row in g.cursor.fetchall()]
+  return (rv[0] if rv else None) if one else rv
+
+@auth.get_password
+def get_password(username):
+    if username == 'glow':
+        return 'python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'message': 'Unauthorized access'}), 403)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+task_fields = {
+    'title': fields.String,
+    'description': fields.String,
+    'done': fields.Boolean,
+    'uri': fields.Url('task')
+}
+
+
+class TaskListAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, required=True,
+                                   help='No task title provided',
+                                   location='json')
+        self.reqparse.add_argument('description', type=str, default="",
+                                   location='json')
+        super(TaskListAPI, self).__init__()
+
+    def get(self):
+        return {'tasks': [marshal(task, task_fields) for task in tasks]}
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        task = {
+            'id': tasks[-1]['id'] + 1,
+            'title': args['title'],
+            'description': args['description'],
+            'done': False
+        }
+        tasks.append(task)
+        return {'task': marshal(task, task_fields)}, 201
+
+
+class TaskAPI(Resource):
+    decorators = [auth.login_required]
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('title', type=str, location='json')
+        self.reqparse.add_argument('description', type=str, location='json')
+        self.reqparse.add_argument('done', type=bool, location='json')
+        super(TaskAPI, self).__init__()
+
+    def get(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        return {'task': marshal(task[0], task_fields)}
+
+    def put(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        task = task[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.items():
+            if v is not None:
+                task[k] = v
+        return {'task': marshal(task, task_fields)}
+
+    def delete(self, id):
+        task = [task for task in tasks if task['id'] == id]
+        if len(task) == 0:
+            abort(404)
+        tasks.remove(task[0])
+        return {'result': True}
+
+
+api.add_resource(TaskListAPI, '/todo/api/v1.0/tasks', endpoint='tasks')
+api.add_resource(TaskAPI, '/todo/api/v1.0/tasks/<int:id>', endpoint='task')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
